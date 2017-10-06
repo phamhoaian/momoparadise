e2a0fbf045b2825c1fa594154e657b3c991f78f3
phamhoaian <phamhoaian005@gmail.com>
2017-10-07 01:21:58 +0700

    adjust branch page

--- html/js/common.js
+++ html/js/common.js
@@ -26,7 +26,30 @@ $(function(){
 	$('.career > dt').click(function(){
 		allPanels.slideUp();
 
-	    $(this).next().slideDown();
-	    return false;
+		if ( ! $(this).hasClass('active'))
+		{
+			$('.career > dt').removeClass('active');
+			$(this).addClass('active');
+			$(this).next().slideDown();
+		}
+		else
+		{
+			$(this).removeClass('active');
+		}
+	});
+
+	$('#btn-apply').click(function(){
+		$(this).slideToggle();
+		$('dl.career').slideToggle();
 	});
 });
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
